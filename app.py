8c8e686ac1f8bcc205a98974aa33ec9dd2de473f
jailchazev <145948981+jailchazev@users.noreply.github.com>
2025-11-09 06:02:39 -0500

    Fix para Render: bind en 0.0.0.0

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
